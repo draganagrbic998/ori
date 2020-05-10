b6ec18b4f0573a339e9eb1373eaf69dade4715b7
rohirim91 <nikolicpetar91@yahoo.com>
2020-05-10 02:29:02 +0200

    Omoguceno ucitavanje slika i obrada i cuvanje slika po pronalazenju optimalnih thresholda. Implementirana Tsallis funkcija i uradjene manje izmene u pso algoritmu

---
+++ projekat1/main.py
@@ -0,0 +1,73 @@
+import dit
+from PIL import Image
+from numpy import asarray
+
+from projekat1 import pso
+
+
+def setup_probs(pixels, level_nums, thresholdi, probs):
+    N = 0
+
+    for i in range(0, len(pixels)):
+        for j in range(0, len(pixels[i])):
+            for k in range(0, len(thresholdi)):
+                if pixels[i][j][0] < thresholdi[k]:
+                    level_nums[k] += 1
+                    break
+            if pixels[i][j][0] >= thresholdi[len(thresholdi) - 1]:
+                level_nums[len(level_nums) - 1] += 1
+            N += 1
+
+    for i in range(0, len(probs)):
+        probs[i] = level_nums[i] / N
+
+
+def tsallis(thresholdi, pixels):
+    level_num = len(thresholdi) + 1
+    level_nums = [0] * level_num
+    probs = [0] * level_num
+
+    level_names = [''] * level_num
+
+    for i in range(0, level_num):
+        level_names[i] = "L" + str(i)
+
+    setup_probs(pixels, level_nums, thresholdi, probs)
+    dist = dit.Distribution(level_names, probs)
+    #order je ono q (index entropije). To se izgleda eskperimentalno podesava. Ja stavio ovo za sad:
+    return dit.other.tsallis_entropy(dist=dist, order=(1 - 1/level_num))
+
+
+def convert_pixels(pixels, thresholdi):
+    for i in range(0, len(pixels)):
+        for j in range(0, len(pixels[i])):
+            for k in range(0, len(thresholdi)):
+                if pixels[i][j][0] < thresholdi[k]:
+                    if k == 0:
+                        pixels[i][j][0:3] = thresholdi[0] / 2
+                    else:
+                        pixels[i][j][0:3] = (thresholdi[k] + thresholdi[k - 1]) / 2
+                    break
+            if pixels[i][j][0] >= thresholdi[len(thresholdi) - 1]:
+                pixels[i][j][0:3] = (255 + thresholdi[len(thresholdi) - 1]) / 2
+    return pixels
+
+
+def main():
+    image = Image.open("images/2.1.05.tiff")
+    pixels = asarray(image)
+    pixels = pixels.copy()
+    pixels.setflags(write=True)
+
+    thresholdi, max = pso.pso(tsallis, pixels=pixels, n_var=3)
+    print(thresholdi)
+    print(max)
+    pixels = convert_pixels(pixels, thresholdi)
+
+    image = Image.fromarray(pixels)
+    image.save("2.1.05.tiff")
+
+
+#ako ovo iz main stavis ovde onda ce ti na nekim mestima pisati da ti parametar fje "shadow-uje" to isto iz sireg opsega
+if __name__ == '__main__':
+    main()
--- projekat1/pso.py
+++ projekat1/pso.py
@@ -2,16 +2,18 @@ import numpy as np
 
 def get_position(n_var):
 
-    lista = [0] * n_var
-    for i in range(n_var):
-        lista[i] = np.random.uniform(-1, 1) #ovo cemo promeniti da ne uzima vrednost od -1 do 1, nego od
+    lista = sorted(np.random.randint(0, 255, n_var))
+
+    #for i in range(n_var):
+    #    lista[i] = np.random.uniform(0, 255) #ovo cemo promeniti da ne uzima vrednost od -1 do 1, nego od
                                             #minimalnog nivoa sivile slike do maksimalnog nivoa sivila slike
     return np.array(lista)
 
-def pso(f, n_var = 1, w = 0.9, wLow = 0.4, cpi = 0.5, cpf = 2.5, cgi = 2.5, cgf = 0.5, particle_num = 100, iter_num = 100, show_iter = 0):
 
+#NE STAVLJAJ PREKO 10 CESTICA i 10 ITERACIJA!!!!!! (sem ako neces da cekas jaaaako dugo)
+def pso(f, pixels, n_var = 1, w = 0.9, wLow = 0.4, cpi = 0.5, cpf = 2.5, cgi = 2.5, cgf = 0.5, particle_num = 5, iter_num = 5, show_iter = 0):
     best_position = None
-    best_value = np.inf
+    best_value = np.inf * (-1)
 
     deltaW = abs(w - wLow) / iter_num * 1.0
     deltaCp = abs(cpi - cpf) / iter_num * 1.0
@@ -25,14 +27,16 @@ def pso(f, n_var = 1, w = 0.9, wLow = 0.4, cpi = 0.5, cpf = 2.5, cgi = 2.5, cgf 
     for i in range(0, particle_num):
 
         pos = get_position(n_var)
-        fpoz = f(pos)
+        fpoz = f(pos, pixels)
 
-        if fpoz < best_value:
+        if fpoz > best_value:
             best_position = pos
             best_value = fpoz
 
+        #pre je ovde stojalo get_position(...) za 'speed'
+        #to sam morao da izmenim jer se desi da mnogo brzo zapuca u 255
         particle = {
-            'speed': get_position(n_var),
+            'speed': 0,
             'position': pos,
             'best_position': pos,
             'value': fpoz,
@@ -44,25 +48,36 @@ def pso(f, n_var = 1, w = 0.9, wLow = 0.4, cpi = 0.5, cpf = 2.5, cgi = 2.5, cgf 
     population = np.array(population)
 
     for i in range(0, iter_num):
-
         for p in population:
-
             r1 = np.random.uniform(0, 1)
             r2 = np.random.uniform(0, 1)
 
             v = w * p['speed'] + cp * r1 * (p['best_position'] - p['position']) + cg * r2 * (best_position - p['position'])
 
             p['speed'] = v
-            p['position'] += v
-            p['value'] = f(p['position'])
+
+            #Lupao mi da ne moze += jer ne moze da pretvori float u int, tako da mora ovo.
+            #Kad probam int() da uradim on mi napravi tipa 123. 234. 12. etc. i opet baca taj error
+            p['position'] = np.add(p['position'], v, casting="unsafe")
+            p['position'] = np.sort(p['position'])
+
+            for j in range(0, n_var):
+                if p['position'][j] > 254:
+                    p['position'][j] = 254
+                elif p['position'][j] < 1:
+                    p['position'][j] = 1
+
+            print(p['position'])
+            p['value'] = f(p['position'], pixels)
 
             if p['value'] > p['best_value']:    #radimo maksimizaciju, pa ne treba da stoji "<"
                 p['best_position'] = p['position']
                 p['best_value'] = p['value']
 
             if p['best_value'] > best_value:    #radimo maksimizaciju pa ne treba da stoji "<"
-                 best_position = p['best_position']
-                 best_value = p['best_value']
+                best_position = p['best_position']
+                best_value = p['best_value']
+                print("New Best: " + str(best_position))
 
         w += deltaW
         cp += deltaCp
@@ -72,6 +87,7 @@ def pso(f, n_var = 1, w = 0.9, wLow = 0.4, cpi = 0.5, cpf = 2.5, cgi = 2.5, cgf 
             print ("Iteration: " + str(i + 1) + ", best value: " + str(best_value))
 
     return best_position, best_value
+
 
 #STA POSLE OVOGA
 
