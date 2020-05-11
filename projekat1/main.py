2e59c18a85130b8c015d7a54d2df244948b88e71
rohirim91 <nikolicpetar91@yahoo.com>
2020-05-11 02:22:13 +0200

    Optimizovano izracunavanje tsallis funkcije. Dodato merenje i ispis vremena rada pso algoritma

--- projekat1/main.py
+++ projekat1/main.py
@@ -1,6 +1,6 @@
 import dit
 from PIL import Image
-from numpy import asarray, zeros
+from numpy import asarray, zeros, where
 import timeit
 from projekat1 import pso
 
@@ -8,13 +8,21 @@ from projekat1 import pso
 def setup_probs(pixels, level_nums, thresholdi):
     N = len(pixels)
 
-    for i in range(N):
-        for j in range(len(thresholdi)):
-            if pixels[i] < thresholdi[j]:
-                level_nums[j] += 1
-                break
-        if pixels[i] >= thresholdi[len(thresholdi) - 1]:
-            level_nums[len(level_nums) - 1] += 1
+    levels = []
+
+    for i in range(len(thresholdi)):
+        if i == 0:
+            levels.append([0, thresholdi[0]])
+        else:
+            levels.append([thresholdi[i - 1], thresholdi[i]])
+
+    levels.append([thresholdi[len(thresholdi) - 1], 255])
+
+    for i in range(len(levels)):
+        if i == 0:
+            level_nums[i] = pixels[(pixels >= levels[i][0]) & (pixels <= levels[i][1])].size
+        else:
+            level_nums[i] = pixels[(pixels > levels[i][0]) & (pixels <= levels[i][1])].size
 
     return level_nums / N
 
@@ -63,7 +71,8 @@ def simplify_pixels(pixels):
 #Nemoj slike vece od 512x512. Dugo ces cekati.
 def main():
     #Pazi: onih 6 iz pdf-a se zavrsavaju na tif a ostale na tiff
-    image = Image.open("images/lena.tif")
+    name = "3.2.25.tiff"
+    image = Image.open("images/" + name)
     pixels = asarray(image)
     pixels = pixels.copy()
     pixels.setflags(write=True)
@@ -71,13 +80,15 @@ def main():
     new_pixels = simplify_pixels(pixels)
 
     start = timeit.default_timer()
-    thresholdi, max = pso.pso(tsallis, pixels=new_pixels, n_var=3, w=0.4, wLow=0.1, cpi=2, cpf=2, cgi=2, cgf=2, particle_num=10, iter_num=10)
-    print(thresholdi)
-    print(max)
+    thresholdi, max = pso.pso(tsallis, pixels=new_pixels, n_var=3, w=0.4, wLow=0.1, cgf=2, cpf=2, cgi=2, cpi=2, particle_num=20, iter_num=100)
+
+    print("Najbolji pragovi: " + str(thresholdi))
+    print("Najbolja vrednost Tsallis funkcije: " + str(max))
+    print("Trajanje: " + str(timeit.default_timer() - start))
+
     pixels = convert_pixels(pixels, thresholdi)
-    print(timeit.default_timer() - start)
     image = Image.fromarray(pixels)
-    image.save("output.tiff")
+    image.save("output/" + name)
 
 
 #ako ovo iz main stavis ovde onda ce ti na nekim mestima pisati da ti parametar fje "shadow-uje" to isto iz sireg opsega
